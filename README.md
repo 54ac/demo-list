ad65fa685b4b4d323a489b8cbb840cf8b15b54ba
54ac <me@54ac.ovh>
2025-04-01 20:17:12 +0200

    update links, move to vite

--- README.md
+++ README.md
@@ -6,8 +6,4 @@
 
 ### What is this?
 
-This is a simple list of demos for my GitHub repos as well as some other projects I did. Uses Javascript to make a list from a JSON file. Bundled with ~~Rollup~~ Parcel.
-
-### What's the point?
-
-It collects some of the stuff I tinkered with into a neat list + it helped me familiarize myself with Rollup and Parcel. Also, I wanted it to be tiny.
+This is a simple list of demos for my GitHub repos as well as some other projects I did.
--- src/main.js
+++ src/main.js
@@ -1,34 +1,11 @@
-import projects from "./projects.json";
+import "./style.css";
 
-const projectsDiv = document.querySelector("#projects");
-
-const me = document.querySelector("#me");
-
-for (const p of projects.sort((a, b) =>
-	a.name.replace("the", "").localeCompare(b.name.replace("the", ""))
-)) {
-	const pA = document.createElement("a");
-	pA.classList.add("mainLink");
-	const pMainP = document.createElement("p");
-	pMainP.classList.add("mainP");
-	const pSecP = document.createElement("p");
-	pSecP.classList.add("secP");
-
-	pA.target = "_blank";
-	pA.rel = "noopener noreferer";
-	pA.href = p.url || `https://${window.location.hostname}:${p.port}`;
-
-	pMainP.textContent = p.name;
-	pSecP.textContent = p.description;
-
-	pA.appendChild(pMainP);
-	pA.appendChild(pSecP);
-	projectsDiv.appendChild(pA);
-}
+const user = "me";
+const domain = "54ac.ovh";
 
 const meA = document.createElement("a");
-const user = "54acbio";
-const domain = "gmail.com";
+
 meA.href = "mailto:" + user + "@" + domain;
 meA.textContent = "54ac";
-me.appendChild(meA);
+
+document.getElementById("me").appendChild(meA);
